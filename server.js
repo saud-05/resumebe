30e74e11d5e208158355818be808c2e56ef4b553
saud-05 <saudhussain003@gmail.com>
2025-07-17 12:52:11 +0530

    sds

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+  origin: 'https://resume-beta-five-32.vercel.app',
+}));
+
 app.use(express.json());
 app.use('/api/resumes', resumeRoute);
 
